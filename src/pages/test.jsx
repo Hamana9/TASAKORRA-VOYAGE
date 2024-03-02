f2df99f6177fd8c56817b0a36ada8b009ed083a6
cheyma2001 <65093603+MedAZIZI@users.noreply.github.com>
2024-03-02 17:10:31 +0100

    cards

---
+++ src/pages/test.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Card } from 'react-bootstrap'; // Assurez-vous d'importer les composants React Bootstrap si vous les utilisez
+
+const generateCards = (cardsData) => {
+    const cards = cardsData.map(card => (
+      <Col
+        key={card.id}
+        xs={6}
+        sm={6}
+        md={6 / cardsData.length}
+        lg={3}
+        xl={3}
+        style={{ marginBottom: "5%", marginLeft: "3%" }}
+      >
+        <Card>
+          <Card.Body>
+            <Card.Title>{card.titre}</Card.Title>
+            <Card.Text>
+              Durée: {card.duree}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
+  
+    return (
+      <Row
+        className="justify-content-center"
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          textAlign: "center",
+        }}
+      >
+        {cards}
+      </Row>
+    );
+  };
+  
+  const Test = () => {
+    const [cardsData, setCardsData] = useState([]);
+  
+    useEffect(() => {
+      // Faites une requête pour obtenir les données JSON
+      fetch('/src/offres.json')
+        .then(response => response.json())
+        .then(data => setCardsData(data))
+        .catch(error => console.error('Erreur lors de la récupération des données:', error));
+    }, []);
+  
+    return (
+      <div 
+      style={{ marginTop: "50%", marginLeft: "3%" }}
+      >
+        {generateCards(cardsData)}
+      </div>
+    );
+  };
+  
+  export default Test;
--- src/routes/HomeRoutes.jsx
+++ src/routes/HomeRoutes.jsx
@@ -3,6 +3,7 @@ import Landing from "../pages/landing";
 import About from "../pages/about";
 import Offres from "../pages/offres";
 import OffresDetail from "../pages/offreDetail";
+import Test from "../pages/test";
 const HomeRoutes = () => {
   return (
     <Routes>
@@ -10,6 +11,7 @@ const HomeRoutes = () => {
       <Route path="apropos" element={<About />} />
       <Route path="offres" element={<Offres />} />
       <Route path="detail" element={<OffresDetail/>} />
+      <Route path="test" element={<Test/>} />
     </Routes>
   );
 };
